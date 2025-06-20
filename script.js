de810251f34b83a3a0ca7709f8b200f74d3116aa
MartyDev7 <jcmartinezhdez2515@gmail.com>
2025-06-20 15:01:41 -0600

    Variables

---
+++ script.js
@@ -0,0 +1,10 @@
+const fecha = document.querySelector('#fecha')
+const lista = document.querySelector('#lista')
+const input = document.querySelector('#input')
+const botonEnter = document.querySelector('#boton-enter')
+
+// funcion agregar tarea
+
+function agregarTarea(tarea){
+    
+}
