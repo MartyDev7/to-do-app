2e6eda5e9a34ed5f6493e1b242c63ed1a8c0e8cb
MartyDev7 <jcmartinezhdez2515@gmail.com>
2025-06-26 23:30:12 -0600

    Accion boton-Enter

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const botonEnter = document.querySelector('#boton-enter')
 // funcion agregar tarea
 
 function agregarTarea(tarea){
-    const elemento = `<li>
+    const elemento = `<li id="elemento">
                     <i class="far fa-circle co" data="realizado" id="0"></i>
                     <p class="text">${tarea}</p>
                     <i class="fas fa-trash de" data="eliminado" id="0"></i> 
@@ -14,3 +14,10 @@ function agregarTarea(tarea){
     lista.insertAdjacentElement("beforeend",elemento)
 }
 
+botonEnter.addEventListener('click', ()=>{
+    const tarea = input.value
+    if(tarea){
+        agregarTarea(tarea)
+    }
+    input.value=''
+})
