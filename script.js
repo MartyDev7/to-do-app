05a84cd6229c344cd45f8c18a49c3dcf43857298
MartyDev7 <jcmartinezhdez2515@gmail.com>
2025-06-27 00:09:44 -0600

    Check - uncheck

--- script.js
+++ script.js
@@ -2,32 +2,53 @@ const fecha = document.querySelector('#fecha')
 const lista = document.querySelector('#lista')
 const input = document.querySelector('#input')
 const botonEnter = document.querySelector('#enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+let id = 0
 
 // funcion agregar tarea
 
-function agregarTarea(tarea){
+function agregarTarea(tarea,id,realizado,eliminado){
+    
+    if(eliminado){
+        return
+    }
+
+    const REALIZADO = realizado ?check : uncheck
+    const LINE = realizado ?lineThrough : ''
+
     const elemento = `<li id="elemento">
-                    <i class="far fa-circle co" data="realizado" id="0"></i>
-                    <p class="text">${tarea}</p>
-                    <i class="fas fa-trash de" data="eliminado" id="0"></i> 
+                    <i class="far ${REALIZADO}" data="realizado" id=${id}></i>
+                    <p class="text ${LINE}">${tarea}</p>
+                    <i class="fas fa-trash de" data="eliminado" id=${id}></i> 
                 </li>`
     lista.insertAdjacentHTML("beforeend",elemento)
 }
 
+
 botonEnter.addEventListener('click', ()=>{
     const tarea = input.value
     if(tarea){
-        agregarTarea(tarea)
+        agregarTarea(tarea,id,false,false)
     }
     input.value=''
+    id++
 })
 
 document.addEventListener('keyup',function(event){
     if(event.key=='Enter'){
         const tarea = input.value
         if(tarea){
-            agregarTarea(tarea)
+            agregarTarea(tarea,id,false,false)
         }
     input.value = ''
+    id++
     }
+})
+
+lista.addEventListener('click', function(event){
+    const element = event.target
+    const elementData = element.attributes.data.value
+
 })
